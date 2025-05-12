178f5cdbb7d7d0cb4c4cc9bafd6a2817828aed06
jeongbin5211 <hjn123493@gmail.com>
2025-05-12 13:53:12 +0900

    백준2438

---
+++ src/Baekjoon2438.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+
+class Baekjoon2438 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= T; i++) {
+            for (int j = 0; j < i; j++) {
+                bw.write("*");
+            }
+            bw.write("\n");
+        }
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
